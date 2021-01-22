28ec6792376c787144a2a79b02b9f9af33b857cc
Sango <sander.martinsgoncalves@gmail.com>
2021-01-22 19:44:55 +0100

    Update Cojoint_analysis.R
    Error corrected. Creating models

--- Script/Cojoint_analysis.R
+++ Script/Cojoint_analysis.R
@@ -19,9 +19,12 @@ laptops = read.csv('./dataset/laptops.csv', sep=";")
 
 #A proper model to fit a variable of this kind is the Multinomial Logit Model, which is an extension of the binomial logit model to the case where the dependent variable has more than two levels.
 
+#How do we check what is the order of the attributes? One alternative is to look at the order of the coeff of our model
+
 #https://www.notion.so/sanmgo/Choice-based-conjoint-analysis-b134f078f00543c1910280c1fe1182fa
   }
 
+#Analysing if the data is balanced
 summary(laptops)
 xtabs(choice ~ Price, data=laptops)
 xtabs(choice ~ RAM, data=laptops)
@@ -35,24 +38,31 @@ xtabs(choice ~ ScreenSize, data=laptops)
 #reordering the attributes and converting it to qualitative. 
 ########Why we need them as qualitative?
 
-laptops$Price <- as.factor(laptops$Price)
-#laptops$Price <- factor(laptops$Price, levels=c("0.7", "1", "1.5", "2")) not sure if we need to add levels for this
-laptops$RAM <- factor(laptops$RAM, levels=c( "4BG", "8GB", "16GB", "32GB" ))
-laptops$Memory <- factor(laptops$Memory, levels=c( "126GB", "256GB", "512GB", "1T" ))
-laptops$Processor <- factor(laptops$Processor, levels=c( "i3", "i5", "i7", "i9" ))
-laptops$Weight <- factor(laptops$Weight, levels=c( "0.8kg", "1kg", "1.2kg", "1.5kg" ))
-laptops$ScreenSize <- factor(laptops$ScreenSize) 
-#laptops$ScreenSize <- factor(laptops$ScreenSize, levels=c("12","13","14", "16"))  not sure if we need to add levels for this
-laptops$alt <- as.factor(laptops$alt) # Not sure if we need to convert alt...
+laptops$Price       <- as.factor(laptops$Price)
+laptops$RAM         <- factor(laptops$RAM, levels=c( "4GB", "8GB", "16GB", "32GB" ))
+laptops$Memory      <- factor(laptops$Memory, levels=c( "126GB", "256GB", "512GB", "1T" ))
+laptops$Processor   <- factor(laptops$Processor, levels=c( "i3", "i5", "i7", "i9" ))
+laptops$Weight      <- factor(laptops$Weight, levels=c( "0.8kg", "1kg", "1.2kg", "1.5kg" ))
+laptops$ScreenSize  <- as.factor(laptops$ScreenSize) 
+laptops$alt         <- as.factor(laptops$alt) # Not sure if we need to convert alt to qualitative
 
-#How do we check what is the order of the attributes?
 
 #Fitting model
-#laptops.mlogit <- dfidx(laptops, idx = list(c("ques", "resp.id"), "alt"), drop.index=F,levels=c("1", "2", "3","4")) #It was the same...
 laptops.mlogit <- dfidx(laptops, idx = list(c("ques", "resp.id"), "alt"), drop.index=F,levels=c(1,2,3,4))
 
+#Creating alternatives for the models to be chosen
 l1 <- mlogit(choice ~ Price + RAM + Memory + Processor + Weight + ScreenSize, data = laptops.mlogit)
 summary(l1)
 
 l2 <- mlogit(choice ~ Price + RAM + Memory + Processor + Weight + ScreenSize | -1, data = laptops.mlogit)
 summary(l2)
+
+#Theory about the model
+{
+#The Estimate column provides the estimated average part worths for each level; they have to be interpreted with respect to the reference level of each attribute
+
+#The order of magnitude of the estimates provides how strong the preferences are. MNL model coefficients are on the logit scale, they tend to range mainly between ???2 and 2
+
+#The Std. Error column gives the level of precision of the estimates and is followed by the z-test statistics and associated p-value indicating the sample evidence against the null hypothesis that the coefficient is equal to zero.
+}
+
